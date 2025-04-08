c175b4a07afda04fa3c8b2c8aaa8f422e2de9f09
dhayananthV2000 <dhayaanand71@gmail.com>
2025-04-08 19:02:52 +0530

    Update app.py

--- ansible-config/webapp/app.py
+++ ansible-config/webapp/app.py
@@ -1,10 +1,19 @@
+
+import logging
 from flask import Flask
 
 app = Flask(__name__)
 
+logging.basicConfig(
+    filename="/var/log/app.log",
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s: %(message)s"
+)
+
 @app.route("/")
-def home():
-    return "Hello from my Dockerized Python app!"
+def hello():
+    app.logger.info("Hello endpoint was hit")
+    return "Hello from Dockerized Python app!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
